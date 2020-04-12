7f2d471a6e54b86d1b126ceaa92f5649c4cc440b
sergejpetkovski <sergej_petkovski@yahoo.com>
2020-04-12 17:19:44 +0200

    lab points adde

--- Student.java
+++ Student.java
@@ -1,17 +1,71 @@
+import java.util.List;
+
 class Student {
-	String index;
-	String firstName;
-	String lastName;
+    String index;
+    String firstName;
+    String lastName;
+    List<Integer> labPoints;
 
-	//TODO constructor
 
-	//TODO seters & getters
+    //TODO constructor
 
-	public double getAverage() {
-		//TODO
-	}
+    public Student(String index, String firstName, String lastName, List<Integer> labPoints) {
+        this.index = index;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.labPoints = labPoints;
+    }
 
-	public boolean hasSignature() {
-		//TODO
-	}
+
+    //TODO seters & getters
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Integer> getLabPoints() {
+        return labPoints;
+    }
+
+    public void setLabPoints(List<Integer> labPoints) {
+        this.labPoints = labPoints;
+    }
+
+    public double getAverage() {
+        //TODO
+        int suma = 0;
+        double prosek = 0;
+        for(i=0; i<labPoints.size(); i++){
+            suma += labPoints.get(i);
+        }
+        prosek = suma / labPoints.size();
+        return prosek;
+    }
+
+    public boolean hasSignature() {
+        //TODO
+        if(labPoints.size() > 8){
+            return true;
+        }
+    }
 }
